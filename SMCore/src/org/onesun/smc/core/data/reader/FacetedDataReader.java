59a7b9aa0c2ccf27f954ee99b6af71163edd4c69
Vikas <vj.vikasjha@gmail.com>
2012-02-24 10:34:35 +0530

    Added functionality of metadata grouping for twitter streaming

---
+++ SMCore/src/org/onesun/smc/core/data/reader/FacetedDataReader.java
@@ -0,0 +1,17 @@
+package org.onesun.smc.core.data.reader;
+
+public class FacetedDataReader extends AbstractDataReader {
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
